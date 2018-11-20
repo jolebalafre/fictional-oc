44d0400665391b87a0a1182bd4d2f81f33497bc1
Jordan FERLICOT <j.ferlicot@highconnexion.com>
2018-11-20 15:33:05 +0100

    Ajout d'un fichier de fonctions utiles

---
+++ src/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function display_view($view_name, $data = array(), $return = false)
+{
+    extract($data);
+
+    ob_start();
+    require_once __DIR__ . '/../views/' . $view_name . '.php';
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    if ($return) {
+        return $content;
+    } else {
+        echo $content;
+        return;
+    }
+}
